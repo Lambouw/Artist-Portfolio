7e269c10b618ff514aa04ca53290e9b7d6192f0a
Malte <jm.lindemann@web.de>
2024-12-03 22:16:17 +0100

    add animations to header an mobile navbar;

--- src/jsx/Page_Components/Header.jsx
+++ src/jsx/Page_Components/Header.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useEffect, useRef } from 'react';
+import { NavLink, useFetcher } from 'react-router-dom';
 
 import '../../styles/css/header.css'
 
 const Header = () => {
+    const headerRef = useRef();
+
+    useEffect(() => {
+        headerRef.current.classList.add("slide-in-header");
+    }, []);
+
     return (
-        <div className="header">
+        <div className="header" ref={headerRef}>
             <NavLink className="header--item" to="/" >Ingrid<span className='header--item--surname'>Lindemann</span></NavLink>
         </div>
     );
