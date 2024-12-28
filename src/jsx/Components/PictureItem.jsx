d70061ac5960d0d315e813f730484f879f208873
Malte <jm.lindemann@web.de>
2024-12-28 19:18:02 +0100

    add shimmer effect to pictureitem;

--- src/jsx/Components/PictureItem.jsx
+++ src/jsx/Components/PictureItem.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import { useEffect, useState } from "react";
+
 import { Item } from "react-photoswipe-gallery";
 
 import "../../styles/css/pictureitem.css";
 
 const PictureItem = ({ item, image }) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+  const [shimmerVisible, setShimmerVisible] = useState(true);
+
+  const handleImageLoad = () => {
+    setTimeout(() => { 
+      setImageLoaded(true); // Set image as loaded when it finishes loading
+      setShimmerVisible(false); // Hide shimmer placeholder
+    }, 500);
+  };
+
   return (
     <div className="pictureitem--container">
       <Item
@@ -14,14 +25,34 @@ const PictureItem = ({ item, image }) => {
       >
         {({ ref, open }) => (
           <span className="pictureitem--container--item">
+            {/* Shimmer */}
+            {shimmerVisible && (
+              <div
+                className={`pictureitem--container--item--shimmer ${
+                  imageLoaded ? "hidden" : ""
+                }`}
+              ></div>
+            )}
+            
+            {/* Image */}
             <img
-              className="pictureitem--container--item--img"
+              className={`pictureitem--container--item--img ${
+                imageLoaded ? "visible" : ""
+              }`}
               ref={ref}
               onClick={open}
               src={image}
               alt={item.name}
+              onLoad={handleImageLoad}
+              loading="lazy"
             ></img>
-            <div className="pictureitem--container--item--description">
+            
+            {/* Description */}
+            <div
+              className={`pictureitem--container--item--description ${
+                imageLoaded ? "visible" : ""
+              }`}
+            >
               <p className="pictureitem--container--item--description--title">
                 {item.name}
               </p>
