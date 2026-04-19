4c081fa01dd80725a26dcff212308280b0aac839
Malte <jm.lindemann@web.de>
2026-04-19 11:22:36 +0200

    🔥 Remove Sitzende

--- src/jsx/Pages/Art/Caststone.jsx
+++ src/jsx/Pages/Art/Caststone.jsx
@@ -1,19 +1,18 @@
-import '../../../styles/css/art.css'
+import "../../../styles/css/art.css";
 
-import PictureItem from "../../Components/PictureItem";
 import GalleryWrapper from "../../Components/GalleryWrapper";
+import PictureItem from "../../Components/PictureItem";
 
 import image1 from "../../../assets/images/webp/caststone/Kleine_Sitzende.webp";
 import image2 from "../../../assets/images/webp/caststone/Stehende_1.webp";
 import image3 from "../../../assets/images/webp/caststone/Stehende_2.webp";
-import image4 from "../../../assets/images/webp/caststone/Sitzende.webp";
-import image5 from "../../../assets/images/webp/caststone/Tanzende.webp";
-import image6 from "../../../assets/images/webp/caststone/Stehende_3.webp";
+import image5 from "../../../assets/images/webp/caststone/Stehende_3.webp";
+import image4 from "../../../assets/images/webp/caststone/Tanzende.webp";
 
 import CaststoneData from "../../../assets/data/caststonedata.json";
 
 const Caststone = () => {
-  const caststoneImages = [image1, image2, image3, image4, image5, image6];
+  const caststoneImages = [image1, image2, image3, image4, image5];
 
   // loop over caststonedata
   let content = [];
@@ -23,7 +22,7 @@ const Caststone = () => {
         key={CaststoneData[i].name}
         item={CaststoneData[i]}
         image={caststoneImages[i]}
-      ></PictureItem>
+      ></PictureItem>,
     );
   }
 
